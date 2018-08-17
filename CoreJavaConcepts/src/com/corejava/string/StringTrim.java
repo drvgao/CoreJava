909abcb121dc1800e9a91618fbd76581b2bb0b63
drvgao <rajesh.durgam@outlook.com>
2018-08-17 16:36:25 +0530

    demo purpose.

--- CoreJavaConcepts/src/com/corejava/string/StringTrim.java
+++ CoreJavaConcepts/src/com/corejava/string/StringTrim.java
@@ -4,5 +4,6 @@ public class StringTrim {
 	public static void main(String[] args) {
 		System.out.println("hello   ".trim());
 		System.out.println("hello 2nd line");
+		System.out.println("hello 3rd line");
 	}
 }
