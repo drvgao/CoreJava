363e395679edcc72f33840c9ee08933e78152857
drvgao <rajesh.durgam@outlook.com>
2018-08-17 15:52:52 +0530

    another blank line added

--- CoreJavaConcepts/src/com/corejava/string/StringTrim.java
+++ CoreJavaConcepts/src/com/corejava/string/StringTrim.java
@@ -3,5 +3,6 @@ package com.corejava.string;
 public class StringTrim {
 	public static void main(String[] args) {
 		System.out.println("hello   ".trim());
+		System.out.println("hello 2nd line");
 	}
 }
